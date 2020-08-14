1ffa65b0227cc4fd24970a085cfdb0fd23531909
devanshi1986 <devanshi.h.trivedi@gmail.com>
2020-08-13 22:37:07 -0600

    first commit of my code

---
+++ src/main/java/com/ta/Demo_GIT/GIT.java
@@ -0,0 +1,18 @@
+package com.ta.Demo_GIT;
+
+public class GIT {
+	
+	//What is repository? --> place where you can store the project or code.
+	
+	//what is GIT? ---> version control system for tracing changes in source code during software development.
+	
+	// in project we can have more than 1 people code can be merged and same can be released
+	 //git --version
+	
+	// cd - Project path -enter
+	//dir-enter
+	//git init
+	//git status
+	//
+
+}
